281438672508c0aafa7953ecde412164848bd07e
Jason Boyle <jb5531@gmail.com>
2023-10-23 22:57:53 -0400

    Ended at 1:15:04

--- my-first-game.py
+++ my-first-game.py
@@ -16,24 +16,39 @@ text_surface = test_font.render('My First Game', False, 'Black')
 
 # I want to make my snail move faster when stretched out and slower when in,
 # with acceleration. Should be doable, even if not in tutorial.
-snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_x_pos = 600
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(bottomright = (600,300))
 
-# STOPPING AT 54:06
+player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80,300))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.MOUSEBUTTONUP:
+            print('mouse up')
+        # if event.type == pygame.MOUSEMOTION:
+        #     if player_rect.collidepoint(event.pos): print('COLLIDED WITH PLAYER')
     
     screen.blit(sky_surface,(0,0))
-    screen.blit(ground_surface,(0, 300))
-    screen.blit(text_surface,(300, 50))
-    snail_x_pos -= 4
-    if snail_x_pos < -100:
-        snail_x_pos = 800
-    screen.blit(snail_surface,(snail_x_pos, 265))
+    screen.blit(ground_surface,(0,300))
+    screen.blit(text_surface,(300,50))
+
+    snail_rect.x -= 4
+    if snail_rect.right <= 0: snail_rect.left = 800
+    screen.blit(snail_surf,snail_rect)
+    screen.blit(player_surf,player_rect)
+
+    # if player_rect.colliderect(snail_rect):
+    #     print('collision')
+
+    # mouse_pos = pygame.mouse.get_pos()
+    # if player_rect.collidepoint(mouse_pos):
+    #     print(pygame.mouse.get_pressed())
+    
+
 
     pygame.display.update()
     clock.tick(60)
