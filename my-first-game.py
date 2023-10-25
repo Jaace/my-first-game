12cf42eb8dc684393f66d227b53c91ba361d20ee
Jason Boyle <jb5531@gmail.com>
2023-10-25 11:59:50 -0400

    Added basic button input, game active loop

--- my-first-game.py
+++ my-first-game.py
@@ -6,13 +6,16 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('My First Game')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+game_active = True
 
 # This is relative to the WORKING DIRECTORY.
 # Running this program outside of that will cause this to fail.
 # TODO: Is there a better way than just using the full path?
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-text_surface = test_font.render('My First Game', False, 'Black')
+
+score_surf = test_font.render('My game', False, (64,64,64))
+score_rect = score_surf.get_rect(center = (400,50))
 
 # I want to make my snail move faster when stretched out and slower when in,
 # with acceleration. Should be doable, even if not in tutorial.
@@ -21,34 +24,46 @@ snail_rect = snail_surf.get_rect(bottomright = (600,300))
 
 player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80,300))
+player_gravity = 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
         if event.type == pygame.MOUSEBUTTONUP:
             print('mouse up')
-        # if event.type == pygame.MOUSEMOTION:
-        #     if player_rect.collidepoint(event.pos): print('COLLIDED WITH PLAYER')
-    
-    screen.blit(sky_surface,(0,0))
-    screen.blit(ground_surface,(0,300))
-    screen.blit(text_surface,(300,50))
 
-    snail_rect.x -= 4
-    if snail_rect.right <= 0: snail_rect.left = 800
-    screen.blit(snail_surf,snail_rect)
-    screen.blit(player_surf,player_rect)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
+                player_gravity = -20
 
-    # if player_rect.colliderect(snail_rect):
-    #     print('collision')
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                player_gravity = -20
+    if game_active:
+        screen.blit(sky_surface,(0,0))
+        screen.blit(ground_surface,(0,300))
+        pygame.draw.rect(screen,'#c0e8ec',score_rect)
+        pygame.draw.rect(screen,'#c0e8ec',score_rect,10)
+        screen.blit(score_surf,score_rect)
 
-    # mouse_pos = pygame.mouse.get_pos()
-    # if player_rect.collidepoint(mouse_pos):
-    #     print(pygame.mouse.get_pressed())
-    
+        snail_rect.x -= 4
+        if snail_rect.right <= 0: snail_rect.left = 800
+        screen.blit(snail_surf,snail_rect)
 
+        # Player
+        player_gravity += 1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 300: player_rect.bottom = 300
+        screen.blit(player_surf,player_rect)
+
+        # collision
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+    else:
+        screen.fill('Yellow')
 
     pygame.display.update()
     clock.tick(60)
